0d5c14340bcd9427e6c34a7bfef2b4568617ee2d
mbelgaid <mohammed-chakib.belgaid@inria.fr>
2021-10-18 10:30:48 +0200

    add : logging and errors handling

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import time
 from queue import Queue
 from threading import Thread
 from time import sleep
@@ -22,6 +23,22 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 # ***** Start of your solution *****
+error = 1e6
+# extra logging functions
+
+
+def timeit(method):
+    """decorator to measuere the execution time of a function"""
+    def timed_function(*args, **kw):
+        tb = time.time_ns()
+        result = method(*args, **kw)
+        te = time.time_ns()
+        logger.info(
+            f"the method {method.__name__} has taken {(te-tb)/1e3 :2.2f} us")
+        return result
+    return timed_function
+
+
 # ******* intialisation ******
 # Intial policy DB is that each product has 50% chance of getting baught
 policyDB["default"] = {"products": np.array(productsDB), "probabilities": np.full(
@@ -32,6 +49,8 @@ policyDB["ko8w8kdmd"] = {"products": np.array(['beef', 'pizza', 'pasta', 'fondue
                          "probabilities": np.array([0.4, 0.3, 0.2, 0.1])}
 
 
+# mainly for tests
+@timeit
 def add_new_product(user, product):
     """
     Add a new product to the list in a such that it has 50% chance to be picked
@@ -43,46 +62,71 @@ def add_new_product(user, product):
     probabilities = update_value(probabilities)
     policyDB[user]["probabilities"] = np.append(probabilities, value)
     policyDB[user]["products"] = np.append(policyDB[user]["products"], product)
-    logger.info(policyDB[user]["probabilities"])
+    logger.info(
+        f"Updating the policy of the user {user} after adding the product {product} to the database  ")
+    assert 1-error <= sum(policyDB[user]["probabilities"]) <= 1+error
 
 
+# same here
 for user in policyDB.keys():
     add_new_product(user, "potato")
 
 
+@timeit
 def update_probabilities(probabilities, index, gain):
+    # to keep the balance of the probabilities
     loss = gain / (len(probabilities)-1)
-    def update_loss(x): return x - loss
+    # lambda function
+    def update_loss(x): return x - loss if x - loss > 0 else 0
     update_loss = np.vectorize(update_loss)
     probabilities = update_loss(probabilities)
     probabilities[index] += gain + loss
+    # assure that always the probabilities are between 0 and 1
+    if probabilities[index] > 1:
+        probabilities[index] = 1
+    # since it is python there is always a marginal error when handling the floats
+    assert 1-error <= sum(probabilities) <= 1+error
     return probabilities
 
 
 def process(element):
-    logger.info(element['id'])
+    logger.info(
+        f"Updating the policy of the user {element['id']} for the product {element['product']}")
+
     userPolicyDB = policyDB[element['id']]
-    logger.info(userPolicyDB["probabilities"])
     purchasedProduct = element['product']
+    # get the index of the probability of the baught product
     purchasedIndex = np.where(
         userPolicyDB["products"] == purchasedProduct)[0][0]
     gain = userPolicyDB["probabilities"][purchasedIndex] * learning_rate/100
-    userPolicyDB["probabilities"] = update_probabilities(
-        userPolicyDB["probabilities"], purchasedIndex, gain)
+    try:
+        userPolicyDB["probabilities"] = update_probabilities(
+            userPolicyDB["probabilities"], purchasedIndex, gain)
+    except AssertionError:
+        # in case there was an error while calculating the probabilities we assure that the sum of all probabilities is 1
+        logger.error(
+            "the new probabilities are wrong we will try to correct them ")
+        collateralError = (
+            userPolicyDB["probabilities"] - 1)/len(userPolicyDB["probabilities"])
 
-    logger.info(sum(userPolicyDB["probabilities"]))
+        correct_error = np.vectorize(
+            lambda x: x - collateralError if x >= collateralError else 0)
+        userPolicyDB["probabilities"] = correct_error(
+            userPolicyDB["probabilities"])
+        # the unfortunate case is where we have sum(probabilities) > 1 and some of the probabilites are 0 in this case this correction wont work and it would be better to devide the error by len([x for x in probabilities if x > o] ) but it will be too much slow and complicated for the code so for the moment i m ignoring it and i just raise an error
+        assert 1-error <= sum(userPolicyDB["probabilities"]) <= 1+error
 
 
+@ timeit
 @ app.route('/<string:id>/products')
 def products(id):
-
     if not (id in policyDB.keys()):
         # default configuration we know nothing about the suer
+        logging.debug(f"adding a new user ({id}) to the policy base")
         policyDB[id] = deepcopy(policyDB["default"])
-
+    # we pick some products and if the all the products in the list fits in one page we will print them all anyway
     shownProducts = np.random.choice(
-        policyDB[id]["products"], size=PageSize, replace=False, p=policyDB[id]["probabilities"])
-
+        policyDB[id]["products"], size=min(PageSize, policyDB[id]["products"]), replace=False, p=policyDB[id]["probabilities"])
     return jsonify(list(shownProducts))
 
 
